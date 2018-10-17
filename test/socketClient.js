940edd56d70e0c62416a9e3654eaef6dc89b0fcc
장혁 <hyuk.jang@smsoft.co.kr>
2018-10-17 18:34:00 +0900

    @ 테스트 socket Client 소스

---
+++ test/socketClient.js
@@ -0,0 +1,12 @@
+const net = require('net');
+
+const connectInfo = { host: 'fp.smsoft.co.kr', port: 8001 };
+
+const client = net.connect(
+  connectInfo,
+  () => {
+    console.log(`connected ${connectInfo.host} ${connectInfo.port} `);
+  },
+);
+
+client.write('hi');
